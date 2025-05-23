ac31c6306bf74e8a90079eb01b7b41487cbfe2e9
krthkak <krthkak@gmail.com>
2025-05-23 16:48:42 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-// app.js
+// server.js
 import http from 'http';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const PORT = process.env.PORT || 8080;
+// __dirname replacement in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello from Node!</h1><p>This is plain HTML.</p>');
-});
+const server = http.createServer(async (req, res) => {
+  let url = ''
+  switch (req.url){
+    case '/':url = '/index.html';
+    break;
+    case '':url = '/index.html';
+    break;
+    case '/about':url = '/about.html';
+    break;
+    case '/contact-me':url = '/contact-me.html';
+    break;
+    default:url = '404.html';
+    break;
+  }
 
-server.listen(port , () => {
+
+  if (url) {
+    try {
+      const filePath = path.join(__dirname, url);
+      const html = await fs.readFile(filePath, 'utf-8');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html);
+    } catch (err) {
+      res.writeHead(500);
+      res.end('Error loading HTML');
+    }
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
+}); 
+
+server.listen(8080, () => {
   console.log('Server running at http://localhost:3000');
 });
